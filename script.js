2e367ceb6c73f781a1a11c4537f6e9a7f5cbfa2e
Roberto Coral <33666303+robertocoral@users.noreply.github.com>
2022-02-07 11:56:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
-        const webmap = new WebMap({
-          portalItem: {
-            // autocasts as new PortalItem()
-            id: "bf8e7c0dc6024742a55e4ab09fd1db36"
-          }
+    require([
+      "esri/Map",
+      "esri/layers/CSVLayer",
+      "esri/views/MapView",
+      "esri/config",
+      "esri/core/urlUtils",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      CSVLayer,
+      MapView,
+      esriConfig,
+      urlUtils
+    ) {
+
+     var url = "https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%202%20-%20ArcGIS%20JavaScript%20API/stl_crime_wgs_84.csv";
+     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
+
+        const template = {
+          title: "Crime Info",
+          content: "Street: {ILEADSStreet}; Crime Code: {Crime}."
+        };
+
+        const csvLayer = new CSVLayer({
+          url: url,
+          popupTemplate: template
         });
-        const view = new MapView({
-          map: webmap,
-          container: "viewDiv"
-        });
+
+        var symbol = {
+          type: "simple-marker", 
+          color: "red"
+        };
+
+      csvLayer.renderer = {
+        type: "simple", // autocasts as new SimpleRenderer()
+        symbol: symbol
+      };
+
+      var map = new Map({
+        basemap: "gray",
+        layers: [csvLayer]
       });
+
+      var view = new MapView({
+        container: "viewDiv",
+        center: [-90.19973698105836, 38.625784113754406],
+        zoom: 10,
+        map: map
+      });
+
+    });
